0d3fe5907957fa50980c99a4ac922c5a8b6eedd0
kuzacollective <hello@mideva.co>
2025-06-29 04:22:32 +0100

    Fix Netlify deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: process.env.NODE_ENV === 'production' ? '/alanula-pros/' : '/',
+  base: "/",
   plugins: [
     react(),
   ],
